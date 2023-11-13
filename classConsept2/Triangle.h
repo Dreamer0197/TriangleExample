1a618642da94f7e99b40ec3004832ef43c3d1a40
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-13 19:32:42 +0300

    Last Situation

--- classConsept2/Triangle.h
+++ classConsept2/Triangle.h
@@ -5,8 +5,118 @@
 //  Created by Kaan Şengün on 13.11.2023.
 //
 
-#ifndef Triangle_h
-#define Triangle_h
+#include <iostream>
+using namespace std;
+
+class Triangle{
+    float sideA,sideB,sideC;
+public:
+    Triangle(float a,float b,float c);
+    ~Triangle();
+    Triangle(const Triangle &oth);
+    bool isTriangle(float a,float b,float c);
+    float getSideA() const{
+        return sideA;
+    }
+    float getSideB() const{
+        return sideB;
+    }
+    float getSideC() const{
+        return sideC;
+    }
+    void setValue(float a,float b,float c);
+    bool isEquilateral();
+    bool isScalene();
+    bool isIsosceles();
+    void triangleType();
+    float calculateArea();
+};
+
+bool Triangle::isTriangle(float a, float b, float c){
+    if(!(abs(b-c)<a)&&(a<(b+c))){
+        cout<<"Invalid A"<<endl;
+        return false;
+    }
+    if(!(abs(a-c)<b)&&(b<(a+c))){
+        cout<<"Invalid B"<<endl;
+        return false;
+    }
+    if(!(abs(a-b)<c)&&(c<(a+b))){
+        cout<<"Invalid C"<<endl;
+        return false;
+    }
+    return true;
+}
 
 
-#endif /* Triangle_h */
+Triangle::Triangle(float a,float b,float c){
+    while(!isTriangle(a, b, c)){
+        cout<<"Please enter sides(a-b-c):"<<endl;
+        cin>>a>>b>>c;
+    }
+    sideA=a;
+    sideB=b;
+    sideC=c;
+    cout<<"Triangle created."<<endl;
+}
+
+Triangle::~Triangle(){
+    cout<<"Triangle destroyed."<<endl;
+}
+
+Triangle::Triangle(const Triangle &oth){
+    sideA=oth.sideA;
+    sideB=oth.sideB;
+    sideC=oth.sideC;
+    cout<<"Triangle copied to target."<<endl;
+}
+
+void Triangle::setValue(float a, float b, float c){
+    while(!isTriangle(a, b, c)){
+        cout<<"Please enter sides(a-b-c):"<<endl;
+        cin>>a>>b>>c;
+    }
+    sideA=a;
+    sideB=b;
+    sideC=c;
+}
+
+bool Triangle::isEquilateral(){
+    return sideA==sideB && sideC==sideB;
+}
+
+bool Triangle::isIsosceles(){
+    if(isEquilateral()){
+        return true;
+    }
+    else{
+        return sideA==sideB|| sideB==sideC|| sideC==sideA;
+    }
+}
+
+bool Triangle::isScalene(){
+    return !isEquilateral();
+}
+
+void Triangle::triangleType(){
+    if(isEquilateral()){
+        cout<<"Equilateral Triangle"<<endl;
+    }
+    else if (isIsosceles()){
+        cout<<"Isosceles Triangle"<<endl;
+    }
+    else{
+        cout<<"Scalene Triangle"<<endl;
+    }
+}
+
+float Triangle::calculateArea(){
+    float area;
+    if(isEquilateral()){
+        area=(pow(sideA, 2)*sqrt(3)/4);
+        return area;
+    }
+    float u=(sideA+sideB+sideC)/2;
+    area=sqrt(u*(u-sideA)*(u-sideB)*(u-sideC));
+    return area;
+}
--- classConsept2/main.cpp
+++ classConsept2/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
+#include "Triangle.h"
+using namespace std;
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    float a,b,c;
+    cout<<"Please enter sides(a-b-c):"<<endl;
+    cin>>a>>b>>c;
+    Triangle triangle1(a,b,c);//constructor
+    Triangle triangle4={a,b,c};//constructor
+    Triangle triangle5(a,b,c);//constructor
+    
+    Triangle triangle2=triangle1;//copy constructor
+    Triangle triangle3(triangle1);//copy constructor
+    
+    cout<<"-Triangle1-"<<endl;
+    cout<<"SideA:"<<triangle1.getSideA()<<endl;
+    cout<<"SideB:"<<triangle1.getSideB()<<endl;
+    cout<<"SideC:"<<triangle1.getSideC()<<endl;
+    cout<<"Area1:"<<triangle1.calculateArea()<<endl;
+    triangle1.triangleType();
+    
+    cout<<"-Triangle2-"<<endl;
+    triangle2.setValue(5,5,5);
+    cout<<"SideA:"<<triangle2.getSideA()<<endl;
+    cout<<"SideB:"<<triangle2.getSideB()<<endl;
+    cout<<"SideC:"<<triangle2.getSideC()<<endl;
+    cout<<"Area2:"<<triangle2.calculateArea()<<endl;
+    triangle2.triangleType();
+    
+    
+    
+    
+    
+    
     return 0;
 }
